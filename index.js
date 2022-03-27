2c73d4d33ff2d6c9837b728931e60de5136d0930
sunmin <dlatjsals123@naver.com>
2022-03-27 17:06:11 +0900

    로그인 구현

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 const config =require('./config/key');
 const {User} =require('./models/User');
 
@@ -10,8 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //application/json -> json파일을 가져와서 분석하기 위하여 추가
 app.use(bodyParser.json());
-
-
+app.use(cookieParser());
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI,
 {
@@ -23,11 +24,11 @@ mongoose.connect(config.mongoURI,
 }).then(()=>{console.log("MongoDB connected...")})
   .catch(err=>console.log(err))
 
-
 app.get('/', (req, res) => {
   res.send('Hello World! ')
 })
 
+//회원가입 Route
 app.post('/register',(req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
@@ -39,9 +40,40 @@ app.post('/register',(req, res) => {
     if(err) return res.json({success:false, err})
     return res.status(200).json({success : true}) //status(200)성공했다는 의미
   }) 
-
 })
 
+//로그인 Route
+app.post('/login',(req, res)=>{
+  // 1. 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  //findOne -> 몽고디비에서 제공하는 함수
+  //(err,userInfo) -> 콜백함수 에러가 발생하면 err에 에러내용이 성공하면 userInfo에 정보가 들어옴
+  User.findOne({email: req.body.email },(err,userInfo)=>{
+    if(!userInfo)//받아온 userInfo가 없다면 맞는 email정보와 맞는 유저가 없다는것
+    {
+      return res.json({
+        loginSuccess : false,
+        message :"제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+     // 2. 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+     userInfo.comparePassword(req.body.password, (err, isMatch)=>{
+      if(!isMatch){
+        return res.json({loginSuccess : false, message: "비밀번호가 틀렸습니다."})
+      }
+    })
+      // 3. 비밀번호까지 맞다면 토큰을 생성하기
+      userInfo.CreateToken((err,user)=>{
+        //res.status(400) 에러가 있다는 의미이고, 에러가 발생했다고 클라이언트에 전달
+        if(err) return res.status(400).send(err);
+
+        //토큰을 저장한다. 어디에? ->쿠키, 로컬스토리지 => 여기서는 쿠키에 저장하는것을 할거임
+        //-> 쿠키에 저장을 하려면 express에서 제공하는 cookieparser를 이용해야함
+         res.cookie("x_auth", user.token)
+         .status(200)
+         .json({loginSuccess:true, userId:user._id})
+      })
+  })
+})
 
 
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt'); 
+const saltRounds = 10;//saltRounds == 몇글자짜리인지-> slat를 먼저 생성 -> Salt를 이용해서 비밀번호를 암호화해야함
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name:{
@@ -31,6 +33,50 @@ const userSchema = mongoose.Schema({
     type : Number
   }
 })
+
+//index.js에 save(유저 정보를 저장하는 함수)가 실행되기 전에 실행됨 
+//-> 이 함수 또한 몽고디비에서 지원하는 함수
+userSchema.pre('save',function(next){
+  var user = this;
+
+  // 패스워드가 변경될때만 비밀번호를 암호화 한다.
+  if(user.isModified('password')){
+      //비밀번호를 암호화 시킨다. -> bcrypt 사이트에 공개되어있는 코드
+     bcrypt.genSalt(saltRounds,function(err,salt){
+      if(err) return next(err);
+
+      bcrypt.hash(user.password,salt,function(err,hash){
+        if(err) return next(err);
+        user.password = hash; // hash된 비밀번호로 비밀번호 교체 
+        next();
+     })
+    })
+  }else{
+    next();
+  }
+})
+
+userSchema.methods.comparePassword = function(plainpassword, callback){
+
+  //plainpassword 1234567 암호화된 비밀번호 $2b$10$WZYd.ZW9W1w1hiAVtmqNsOHFInCWddIWMzWpssWdLulv5TXGhzR6.가 같은지 체크
+  bcrypt.compare(plainpassword,this.password, function(err,isMatch){
+    if(err) return callback(err),
+    callback(null,isMatch)
+  })
+}
+
+userSchema.methods.CreateToken = function(callback){
+  let user = this;
+  //jsonwebtoken을 이용해서 token을 생성
+  let token = jwt.sign(user._id.toHexString(),'secretToken');
+  //user._id+'secretToken' = token 
+  //=> 유저 id와 시크릿토큰을 합쳐서 토큰을 생성 -> 이후에 토큰을 이용하여 유저id를 가져올 수 있다.
+  user.token = token;
+  user.save(function(err,user){
+    if(err) return callback(err);
+    callback(null, user);
+  })
+}
 
 const User = mongoose.model('User', userSchema) // Model은 스키마를 감싸주는 역할을 한다
 
